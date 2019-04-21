9d7e159e0a58f96c3cfd7df0f447396dd1e98b88
msegedyn <researcher2312@gmail.com>
2019-04-21 23:27:45 +0200

    added methodes implementation

--- inc/graph.h
+++ inc/graph.h
@@ -1,11 +1,14 @@
 #ifndef GRAPH_H
 
+
 class GraphArray{
 public:
 	GraphArray(int size);
 	void AddEdge(int vertice1, int vertice2, int weight);
+	void Write() const;
 
 private:
+	int size_;
 	int** array_;
 };
 
--- src/graph.cpp
+++ src/graph.cpp
@@ -1,8 +1,26 @@
 #include "graph.h"
 
-GraphArray::GraphArray(int size){
+#include <iostream>
+
+using namespace std;
+
+GraphArray::GraphArray(int size): size_(size){
 	array_ = new int* [size];
 	for (int i = 0; i < size; i++) {
 		array_[i] = new int [size]{0};
+	}
+}
+
+void GraphArray::AddEdge(int vertice1, int vertice2, int weight){
+	array_[vertice1][vertice2] = weight;
+	array_[vertice2][vertice1] = weight;
+}
+
+void GraphArray::Write() const{
+	for (int i = 0; i < size_; i++){
+		for (int j = 0; j < size_; j++){
+			cout << array_[i][j] << ' ';
+		}
+		cout << '\n';
 	}
 }
