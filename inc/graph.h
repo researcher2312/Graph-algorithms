a6711f087513bd078736d289491011c07df608c5
msegedyn <researcher2312@gmail.com>
2019-05-01 12:51:27 +0200

    changed methodes in GraphList to use new element classes

--- inc/graph.h
+++ inc/graph.h
@@ -1,6 +1,21 @@
 #ifndef GRAPH_H
+#define GRAPH_H
 
 #include "list.h"
+#include "graph_elements.h"
+
+class GraphList{
+public:
+	GraphList (int size);
+	void AddEdge(int vertice1, int vertice2, int weight);
+	void Write() const;
+	bool IsConnected(int vertice1, int vertice2);
+
+private:
+	int size_;
+	GraphVertice* graph_vertice_list_;
+	List<GraphEdge*> graph_edge_list_;
+};
 
 class GraphArray{
 public:
@@ -12,18 +27,6 @@ public:
 private:
 	int size_;
 	int** array_;
-};
-
-class GraphList{
-public:
-	GraphList (int size);
-	bool AddEdge(int vertice1, int vertice2, int weight);
-	void Write() const;
-	bool IsConnected(int vertice1, int vertice2);
-
-private:
-	int size_;
-	List<int>* list_;
 };
 
 #define GRAPH_H
--- src/graph.cpp
+++ src/graph.cpp
@@ -30,25 +30,22 @@ void GraphArray::Write() const{
 }
 
 bool GraphArray::IsConnected (int vertice1, int vertice2) {
-	if(array_[vertice1][vertice2] > 0)
-		return 1;
-	else
-		return 0;
+	return array_[vertice1][vertice2] > 0;		
 }
+
+
 
 GraphList::GraphList(int size): size_(size){
-	list_ = new List<int>[size];
+	graph_vertice_list_ = new GraphVertice[size_];
 }
 
-bool GraphList::AddEdge(int vertice1, int vertice2, int weight){
+void GraphList::AddEdge(int vertice1, int vertice2, int weight){
 	if (vertice1 < size_ && vertice2 < size_) {
-		list_[vertice1].push_back(vertice2);
-		list_[vertice2].push_back(vertice1);
-		return 1;
+
+			
 	}
-	return 0;
 }
 
 bool GraphList::IsConnected(int vertice1, int vertice2){
-	return list_[vertice1].contains(vertice2);
+	return true; 
 }
