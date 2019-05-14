aff3ff4695c6bee0620494cc92e3dce67b1b669a
msegedyn <researcher2312@gmail.com>
2019-05-15 01:59:08 +0200

    added 100 graph generation

--- src/main.cpp
+++ src/main.cpp
@@ -15,20 +15,25 @@ int main(){
 	int fills [] = {25, 50, 75, 100};
 
 	timeval t1, t2;
- 	double elapsedTime;
+	double totalTime;
+	double elapsedTime;
 
 	for (GraphType current_graph: graph_types) {
 		for (int current_fill: fills) {
 			for (int currennt_size: sizes) {
-				Graph* graph = GenerateGraph(currennt_size, current_fill, current_graph);
-				gettimeofday(&t1, NULL);
-				Dijkstra(graph, 90);
-				gettimeofday(&t2, NULL);
-				elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;      // sec to ms
-   				elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;   //us to ms
-   				cerr << current_graph << ' ' << current_fill << ' ' << currennt_size << ' ';
-   				cerr << elapsedTime <<'\n';
-   				delete graph;
+				totalTime = 0;
+				for (int i = 0; i < 100; i++){
+					Graph* graph = GenerateGraph(currennt_size, current_fill, current_graph);
+					gettimeofday(&t1, NULL);
+					Dijkstra(graph, 90);
+					gettimeofday(&t2, NULL);
+					elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;      // sec to ms
+					elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;   //us to ms
+					totalTime += elapsedTime;
+					delete graph;
+				}
+				cerr << current_graph << ' ' << current_fill << ' ' << currennt_size << ' ';
+				cerr << elapsedTime/100 <<'\n';
 			}
 		}
 	}
